062bb5b0a829591873c0c9376f87997054e15162
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-03-13 09:22:33 -0300

    correção do ts

--- WinFormsApp1/obj/ArtsystemSuporteTs/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/ArtsystemSuporteTs/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ArtsystemSuporteTs")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e54971e998c5ae83e0e86eef8a089ce1c79a53ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca151b2eb9aebd5b319d12abab5e4cf4e57a8c57")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
